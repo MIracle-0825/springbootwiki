621988244651bdda72d6d840c6ecb3acc7081995
Miracle <18435182312@163.com>
2022-10-06 20:52:48 +0800

    init

---
+++ src/main/java/com/example/springbootwiki/SpringbootwikiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootwiki/SpringbootwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
