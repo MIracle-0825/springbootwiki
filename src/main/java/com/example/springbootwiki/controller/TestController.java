a39cea91743668e8f6a2d262a165a3f8d3f567f2
Miracle <18435182312@163.com>
2022-10-06 22:06:56 +0800

    testController

---
+++ src/main/java/com/example/springbootwiki/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.example.springbootwiki.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController//通常返回字符串
+public class TestController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
